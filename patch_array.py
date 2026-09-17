375b95340bb8caa21627036cbd5e52d832d78994
Emmanuel Baraka <wakilibar@gmail.com>
2026-09-17 15:34:18 +0300

    chore: Repo hygiene pass (remove agent scratch files and dead submodules)

--- patch_array.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-with open('Sources/DeskBar/Views/TaskbarContentView.swift', 'r') as f:
-    content = f.read()
-
-content = content.replace("            runningAppTrayView,\n", "            runningAppTrayView as NSView?,\n")
-
-with open('Sources/DeskBar/Views/TaskbarContentView.swift', 'w') as f:
-    f.write(content)
--- update_dock_layout.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# First, we need to inspect TaskbarContentView.swift to safely write a sed/patch to remove RunningAppTrayView
-cat Sources/DeskBar/Views/TaskbarContentView.swift | grep -n "runningAppTrayView"
